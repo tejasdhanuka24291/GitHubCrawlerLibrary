2853bf2b125a80787d154fde17da12007279035a
Tejas, Dhanuka <ts-dhanuka.tejas@rakuten.com>
2019-12-11 18:27:35 +0900

    Initial Project Setup

---
+++ GitHubCrawlerLibrary/GitHubCrawlerLibrary.h
@@ -0,0 +1,19 @@
+//
+//  GitHubCrawlerLibrary.h
+//  GitHubCrawlerLibrary
+//
+//  Created by Tejas, Dhanuka on 2019/12/11.
+//  Copyright © 2019 Tejas, Dhanuka. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GitHubCrawlerLibrary.
+FOUNDATION_EXPORT double GitHubCrawlerLibraryVersionNumber;
+
+//! Project version string for GitHubCrawlerLibrary.
+FOUNDATION_EXPORT const unsigned char GitHubCrawlerLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GitHubCrawlerLibrary/PublicHeader.h>
+
+
